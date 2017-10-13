ac46cb571722f117acdef75bf293702e752c5652
Annie Chiang <devbear22@gmail.com>
2017-10-13 10:51:44 -0700

    changed export

--- models.js
+++ models.js
@@ -2,15 +2,16 @@
 
 const mongoose = require('mongoose');
 
+//in schema, left side = property; right side is the metadata 
 const blogPostSchema = mongoose.Schema ({
-  author: {firstName: String, lastName: String},
-  title: String,
-  content: {type: String}, //what's the difference in syntax for title vs content? 
-  created: {type: Date, default: Date.now},
+  author: {firstName: String, lastName: String}, //author has 2 properties
+  title: String, //property: data type
+  content: {type: String}, //using 1 config option - meta data about those fields 
+  created: {type: Date, default: Date.now}, //property: {define the objects to pass it in}
 });
 
-//return author proprety as one string  (virtual property)
-blogPostSchema.virtual('author').get(function() {
+//return author property as one string  (virtual property)
+blogPostSchema.virtual('authorName').get(function() {
   return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
@@ -24,5 +25,5 @@ blogPostSchema.methods.showAuthor = function() {
   };
 };
 
-const BlogPosts = mongoose.model(blogPosts, blogPostSchema);
-module.exports = {BlogPosts}; //?
+const BlogPosts = mongoose.model('blogPosts', blogPostSchema);
+module.exports = BlogPosts; // use this as an object when you are exporting multiple things
